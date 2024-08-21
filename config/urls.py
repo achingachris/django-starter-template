d016156d2b433ec9b26ef4cf325378a3d0721201
C A <achinga.chris@gmail.com>
2024-08-21 18:56:07 +0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-
+app_name = "config"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
